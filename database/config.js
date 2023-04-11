b633f7018176ff1c45946a29ae32a910ad4ec6d0
bilvinas.ivan@gmail.com <bilvinas.ivan@gmail.com>
2023-04-11 14:38:09 -0300

    fix/db connection

--- database/config.js
+++ database/config.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
-const dbConnection = async() => {
+const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.DB_CNN);
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('DB Online')
     } catch (error) {
         console.log(error)
